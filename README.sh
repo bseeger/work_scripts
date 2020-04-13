edb5e8ad7f7ab7c675ecc49b20181348067f6e77
bseeger <bseeger@jhu.edu>
2020-04-13 09:33:33 -0400

    Adds readme

---
+++ README.sh
@@ -0,0 +1,7 @@
+
+# Work Scripts
+
+This repository contains random scripts that I've written in various languages to do various, random, probably not repeatable things. 
+Namely, it's a place where I'm putting one-off scripts that I may or may not use again and you're welcome to borrow from.
+
+Enjoy!
